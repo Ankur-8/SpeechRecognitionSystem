c52e83b11e525dd8e9ef9383240ecfd73d420a54
Ankur Bhardwaj <ankurb75@gmail.com>
2024-06-21 15:03:41 -0700

    added main file

---
+++ main.py
@@ -0,0 +1,5 @@
+import audio_record
+import speech_to_text
+
+audio_record.record_audio()
+speech_to_text.transcribe()
--- speech_to_text.py
+++ speech_to_text.py
@@ -3,10 +3,11 @@
 
 import assemblyai as aai
 
-aai.settings.api_key = "39acb8bbe99041e8a873e7c487969b88"
-transcriber = aai.Transcriber()
+def transcribe():
+    aai.settings.api_key = "39acb8bbe99041e8a873e7c487969b88"
+    transcriber = aai.Transcriber()
 
-# transcript = transcriber.transcribe("https://storage.googleapis.com/aai-web-samples/news.mp4")
-transcript = transcriber.transcribe("./input.wav")
+    # transcript = transcriber.transcribe("https://storage.googleapis.com/aai-web-samples/news.mp4")
+    transcript = transcriber.transcribe("./input.wav")
 
-print(transcript.text)
+    print(transcript.text)
